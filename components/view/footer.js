567d148472b3c48f9357d24c86b49e26bf800b23
samuel <shopifyworld28@gmail.com>
2022-05-10 19:10:35 -0700

    first commit

---
+++ components/view/footer.js
@@ -0,0 +1,10 @@
+import html from "html-literal";
+
+let year = new Date() .getFULLYEAR();
+
+export default ()=> html
+
+<footer>
+&copy; ${year} <a href="https://savvycoders.com/">Savvy Coders</a>
+</footer>
+;
